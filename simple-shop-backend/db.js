1467c89c19997399a5cce05dd6a746957a735ae6
Oleh Shevchenko <88991654+Alex55282005@users.noreply.github.com>
2025-04-23 03:01:33 +0300

    Update db.js

--- simple-shop-backend/db.js
+++ simple-shop-backend/db.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',   // или 'localhost'
-  port: 3306,         // Порт MySQL, обычно 3306
-  user: 'root',       // Ваш пользователь MySQL
-  password: 'qwerty', // Если пароля нет, укажите ''
-  database: 'simple_shop',       // Название вашей БД
+  host: '127.0.0.1',  
+  port: 3306,         
+  user: 'root',      
+  password: 'qwerty', 
+  database: 'simple_shop',  
   connectionLimit: 10,
 });
 
